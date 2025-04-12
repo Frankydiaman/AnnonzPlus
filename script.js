055b4b488a26295027397b36c2b03318a1200691
Frankydiaman <abdoulhadjcoulibaly@gmail.com>
2025-04-12 02:40:40 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const adForm = document.getElementById("adForm");
+const adList = document.getElementById("adList");
 
+adForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const price = document.getElementById("price").value;
+  const contact = document.getElementById("contact").value;
+  const desc = document.getElementById("desc").value;
+
+  const ad = document.createElement("div");
+  ad.classList.add("ad");
+  ad.innerHTML = `
+    <h3>${title} - ${price} FCFA</h3>
+    <p>${desc}</p>
+    <a href="https://wa.me/${contact}" target="_blank">Contacter sur WhatsApp</a>
+  `;
+
+  adList.prepend(ad);
+  adForm.reset();
+});
